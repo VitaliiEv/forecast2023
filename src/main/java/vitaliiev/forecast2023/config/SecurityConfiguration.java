1bb9f2fe13df7feea7609b484d8190cdccb6eff9
Vitalii Solomonov <solomonov.v@gmail.com>
2023-03-06 20:09:53 +0300

    disable csrf for REST API

--- src/main/java/vitaliiev/forecast2023/config/SecurityConfiguration.java
+++ src/main/java/vitaliiev/forecast2023/config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf()
+                .ignoringRequestMatchers("/api/**")
                 .ignoringRequestMatchers("/h2-console/**")
                 .and()
                 .authorizeHttpRequests()
